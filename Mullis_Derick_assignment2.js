36b77d42c6754b4f3fc6183ece289d0f5e1693a5
Derick Mullis <dmullis5@fullsail.edu>
2013-04-17 21:35:44 -0400

    Added an additional boolean function
    To add to the story flow and provide another boolean function

--- Mullis_Derick_assignment2.js
+++ Mullis_Derick_assignment2.js
@@ -1,17 +1,25 @@
 //alert("JavaScript works!");
+// Derick Mullis
+// April 18, 2013
+// Project 2
+// This script is for completion of project 2.  Heroes are going monster hunting... or are they?
 
+
+//Variables
 var heroes = ["TehKuv", "SquishyDingo", "Vaus"]
 var monster = "Baron Nashor"
 var potions = "health"
 var heroHealthPoints = 100
 var monsterHealthPoints = 500
-var numberOfPotions = 5
+var amountOfPotions = 5
+var heroDamage = 50
+var weaponsReady = true
 
 
 //Procedure
 var letsGoAdventuring = function(feelingAdventurous) {
 	if ( feelingAdventurous === "yes" ) {
-		console.log( "We are feeling adventurous, are there any monsters to fight?" );
+		console.log( "We are feeling adventurous, let's check our gear." );
 	} else {
 		console.log( "Let's check our health and potions before we head out." );
 	};
@@ -23,8 +31,13 @@ letsGoAdventuring("yes")
 
 // Boolean Function
 var readyCheck = function (health, numberOfPotions) {
-	if ( health === 100 && numberOfPotions === 5 ) {
-		console.log( "Ready check complete let's go find a monster!" );
+	if ( health === 100 && numberOfPotions >= 5 ) {
+		console.log( "Ready check complete." );
+		if ( weaponsReady === true ) {
+			console.log ( "Our weapons are ready and yearn for battle!" );
+		} else {
+			console.log ( "We need to condition our weapons before heading out." );	
+		};
 	} else {
 		console.log( "On 2nd thought we better not go out adventuring today." );
 	};
@@ -32,7 +45,7 @@ var readyCheck = function (health, numberOfPotions) {
 	return status;
 };
 
-var status = readyCheck(100, 5);
+var status = readyCheck(100, 4);
 console.log(status);
 
 
