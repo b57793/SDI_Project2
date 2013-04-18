5119afb67165afe70b241928510b1d6448ed78e2
Derick Mullis <dmullis5@fullsail.edu>
2013-04-17 20:40:42 -0400

    Added procedure and boolean function
    Worked for far too long without commiting.

--- Mullis_Derick_assignment2.js
+++ Mullis_Derick_assignment2.js
@@ -1,1 +1,44 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+var heroes = ["TehKuv", "SquishyDingo", "Vaus"]
+var monster = "Baron Nashor"
+var potions = "health"
+var heroHealthPoints = 100
+var monsterHealthPoints = 500
+var numberOfPotions = 5
+
+
+//Procedure
+var letsGoAdventuring = function(feelingAdventurous) {
+	if ( feelingAdventurous === "yes" ) {
+		console.log( "We are feeling adventurous, are there any monsters to fight?" );
+	} else {
+		console.log( "Let's check our health and potions before we head out." );
+	};
+};
+
+// Boolean Function
+var readyCheck = function (health, potions) {
+	if ( health === 100 && potions === 5 ) {
+		console.log( "Ready check complete let's go find a monster to fight!" );
+	} else {
+		console.log( "On 2nd thought we better not go out adventuring today." );
+	};
+	noAdventure = ( health <= 99 || potions <= 4 );
+	return noAdventure;
+};
+
+//String Function
+
+
+
+
+
+
+
+
+
+
+
+letsGoAdventuring("yes")
+readyCheck(100, 5);
