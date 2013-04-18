d12270c7fd0889298fd2a0c4e88601ce171857f5
Derick Mullis <dmullis5@fullsail.edu>
2013-04-18 14:13:47 -0400

    Final!!!!
    Wow, this has been an amazing 2 days.  All returned values have been
    updated.

--- Mullis_Derick_assignment2.js
+++ Mullis_Derick_assignment2.js
@@ -8,7 +8,6 @@
 //Variables
 var monster = "Baron Nashor"
 var potions = "health"
-var heroHealthPoints = 100
 var amountOfPotions = 5
 var weaponsReady = true
 
@@ -23,28 +22,28 @@ var letsGoAdventuring = function(feelingAdventurous) {
 	};
 };
 
-letsGoAdventuring("yes")
 
 
 
 // Boolean Function
 var readyCheck = function(health, numberOfPotions) {
 	if ( health === 100 && numberOfPotions >= 5 ) {
-		console.log( "Ready check complete. We should be good to go." );
+		console.log( "Ready check complete. We should be good to go." )
 		if ( weaponsReady === true ) {
 			console.log ( "Our weapons are ready and yearn for battle!" );
 		} else {
-			console.log ( "Well actually we need to condition our weapons before heading out." );	
+			console.log ( "Well actually we need to condition our weapons before heading out." );
 		};
 	} else {
-		console.log( "We should rest up to 100 health and make sure we have at least 5 " + potions + " potions before we head out." );
+		console.log( "We should rest up to 100 health and make sure we have at least " + amountOfPotions+ " " + potions + " potions before we head out." );
 	};
-	var update = ( "All hero's health at " + heroHealthPoints + " and we have " + amountOfPotions + " " + potions + " potions we are ready to depart."  );
-	return update;
+		if ( health === 100 && numberOfPotions === 5 ) {
+			return (true);
+		} else {
+			return (false);
+		};
 };
 
-var status = readyCheck(100, 5);
-console.log(status);
 
 
 
@@ -53,9 +52,6 @@ var baronAppears = function(travelDistance, monster) {
 	var monsterFight = ( "After leaving town and traveling " + travelDistance + ". We encountered " + monster + "!" );
 	return monsterFight;
 };
-
-var itsTime = baronAppears("a fortnight", "Baron Nashor");
-console.log(itsTime);
 
 
 
@@ -72,9 +68,11 @@ var baronFight = function(baronHP) {
 				console.log( monster + " has been slain.");
 			};
 		};
+		var BaronLife = baronHP
+		return BaronLife		
 };
 
-baronFight(500);
+
 
 //Array Function
 var chests = function(argArray, number) {
@@ -89,6 +87,30 @@ var chests = function(argArray, number) {
 	for ( var i= 0;  i <= chestTypes; i++) {
 		console.log("Huzzah! Look at all the loot! " + argArray[i] + " chests for everyone!");
 	};
+	var updatedArray = argArray
+	return updatedArray
 };
 
-chests(["Gold", "Diamond", "Emerald"], 10)  
+
+
+
+
+//Main Code
+letsGoAdventuring("yes")
+var status = readyCheck(100, 5);
+var itsTime = baronAppears("a fortnight", "Baron Nashor");
+var baronLife = baronFight(500);
+var updatedChests = chests (["Gold", "Diamond", "Emerald"], 10);  
+
+
+//Returned Values
+console.log( " " );
+console.log("Return Values:");
+//Return for Boolean
+	console.log(status);
+//Return for String
+	console.log(itsTime);
+//Return for Number
+	console.log(baronLife);
+//Return for Array
+	console.log(updatedChests);
